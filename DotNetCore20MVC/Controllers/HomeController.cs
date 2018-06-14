e10a659f59561469c47df433ee29d79ee4f1f3ff
Jeremy Brooks <lilman_2010@hotmail.com>
2018-06-13 21:30:29 -0400

    First Commit
    First Commit

--- DotNetCore20MVC/Controllers/HomeController.cs
+++ DotNetCore20MVC/Controllers/HomeController.cs
@@ -5,18 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DotNetCore20MVC.Models;
+using Microsoft.Extensions.Logging;
 
 namespace DotNetCore20MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("TEST INDEX LOGGER");
+
             return View();
         }
 
         public IActionResult About()
         {
+            _logger.LogInformation("TEST ABOUT LOGGER");
             ViewData["Message"] = "Your application description page.";
 
             return View();
@@ -24,6 +35,7 @@ namespace DotNetCore20MVC.Controllers
 
         public IActionResult Contact()
         {
+            _logger.LogInformation("TEST CONTACT LOGGER");
             ViewData["Message"] = "Your contact page.";
 
             return View();
--- DotNetCore20MVC/Startup.cs
+++ DotNetCore20MVC/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DotNetCore20MVC
 {
@@ -25,7 +26,7 @@ namespace DotNetCore20MVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -38,6 +39,9 @@ namespace DotNetCore20MVC
             }
 
             app.UseStaticFiles();
+
+            loggerFactory.AddDebug();
+            loggerFactory.AddAzureWebAppDiagnostics();
 
             app.UseMvc(routes =>
             {
